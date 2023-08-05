589620ff0f7b39bc1dd9c5b7ca5a0ad74124094a
bachiito <riveraabraham332@gmail.com>
2023-08-05 18:31:37 -0400

    country model updated

--- src/app/models/country.model.ts
+++ src/app/models/country.model.ts
@@ -1,15 +1,52 @@
 export interface Country {
-  name: string;
-  nativeName: string;
-  population: number;
-  region: string;
-  subRegion: string;
-  capital: string;
-  topLevelDomain: string[];
-  currencies: Currency[];
-  languages: Language[];
+  altSpellings?: string[];
+  area?: number;
   borders: string[];
-  flag: string;
+  capital: string[];
+  capitalInfo?: CapitalInfo;
+  car?: Car;
+  cca2?: string;
+  cca3?: string;
+  ccn3?: string;
+  cioc?: string;
+  coatOfArms?: CoatOfArms;
+  continents?: string[];
+  currencies: Record<string, Currency>[];
+  demonyms?: Record<string, Demonyms>;
+  fifa?: string;
+  flag?: string;
+  flags?: Flags;
+  idd?: Idd;
+  independent?: boolean;
+  landlocked?: boolean;
+  languages: Record<string, string>;
+  latlng?: number[];
+  maps?: Maps;
+  name: Name<Translations>;
+  population: number;
+  postalCode?: PostalCode;
+  region: string;
+  startOfWeek?: string;
+  status?: string;
+  subRegion: string;
+  timezones?: string[];
+  tld: string[];
+  translations?: Record<string, Translations>;
+  unMember?: boolean;
+}
+
+interface CapitalInfo {
+  latlng: number[];
+}
+
+interface Car {
+  side: string;
+  signs: string[];
+}
+
+interface CoatOfArms {
+  png: string;
+  svg: string;
 }
 
 interface Currency {
@@ -18,9 +55,37 @@ interface Currency {
   symbol: string;
 }
 
-interface Language {
-  iso639_1: string;
-  iso639_2: string;
-  name: string;
-  nativeName: string;
+interface Demonyms {
+  f: string;
+  m: string;
+}
+
+interface Flags extends CoatOfArms {
+  alt: string;
+}
+
+interface Idd {
+  root: string;
+  suffixes: string[];
+}
+
+interface Maps {
+  googleMaps: string;
+  openStreetMaps: string;
+}
+
+interface Name<T> extends Translations {
+  nativeName: {
+    [key: string]: T;
+  };
+}
+
+interface PostalCode {
+  format: string;
+  regex: string;
+}
+
+interface Translations {
+  official: string;
+  common: string;
 }
